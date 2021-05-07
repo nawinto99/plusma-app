2e4ce78b7a153aa1f7b3db704f805218dab48c0c
Naveen Thurimerla <nawinto99@gmail.com>
2021-05-07 15:15:03 +0530

    fix covid dashboard data refresh

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-      <meta charSet="utf-8"></meta>
+        <meta charSet="utf-8"></meta>
         <title>Plusma</title>
         <meta name="Description" content="The platform connects donors and seekers." />
-        <meta name="author" content="Naveen Thurimerla"></meta>
+        <meta name="author" content="Naveen Thurimerla" />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, Plusma, NextJS, React, Naveen Thurimerla, TailwindCSS"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YG2HZGLRBD"></script>
         <script
           dangerouslySetInnerHTML={{
