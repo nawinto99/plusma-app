ea5e089a0eed3f1e5a0546039936549ef3414b46
Naveen Thurimerla <nawinto99@gmail.com>
2021-05-09 13:20:10 +0530

    Split Code for Donor and Seeker

---
+++ components/BloodSeeker.js
@@ -0,0 +1,15 @@
+export  function BloodSeeker() {
+    return (
+        <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
+        <div className="max-w-md">
+          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Work In-Progress.</p>
+          <Link href="/">
+            <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
+              Back to homepage
+            </button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import {BloodDonorIndex, BloodSeekerIndex} from "@/pages/blood"
+
+
 export default function Index() {
+  
+  
   return (
     <div className="flex items-center justify-center">
       <div className="grid grid-cols-1 gap-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
@@ -25,14 +30,16 @@ export default function Index() {
           </div>
           <div className="border-t-4"></div>
           <div className="mt-8">
-            <a href="blood">
-              <button
+          <a href="blood" onClick={() => { BloodSeekerIndex(); }}> 
+          <button
                 className="bg-pink-400 text-white active:bg-pink-600 font-bold uppercase text-sm px-14 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
               >
                 <span>Donor</span>
               </button>
-            </a>
+        </a>
+            
+            
 
             <button
               className="bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-sm px-14 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
