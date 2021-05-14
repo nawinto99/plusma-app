db1c46bd1162fb30ddb2dd32ddd750ff956db455
Naveen Thurimerla <nawinto99@gmail.com>
2021-05-15 00:24:40 +0530

    fix blood seeker search

--- pages/bloodseeker.js
+++ pages/bloodseeker.js
@@ -1,83 +1,72 @@
 import { BloodSeeker } from '@/components/BloodSeeker'
-import { dataStates } from '@/components/Data'
+import React, { useState } from 'react'
+import Select from 'react-select'
+import data from '@/data/data_india_state_city.json'
 
+export default function BloodSeekerIndex(props) {
+  const [state, setState] = useState(null)
+  const [city, setCity] = useState(null)
+  const [cityList, setCityList] = useState([])
 
+  const styles = {
+    fontSize: 14,
+    color: 'blue',
+  }
+  const handleStateChange = (obj) => {
+    setState(obj)
+    setCityList(obj.cityList)
+    setCity(null)
+  }
 
-export default function BloodSeekerIndex() {
+  const handleCityChange = (obj) => {
+    setCity(obj)
+  }
 
   return (
     <>
-    <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <div className="sm:mt-0">
           <div className="md:grid">
             <div className="md:mt-0 md:col-span-1">
-              <form >
+              <form>
                 <div className="shadow overflow-hidden rounded-lg">
                   <div className="px-4 py-5 bg-white sm:p-6">
                     <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3 lg:col-span-6">
+                      <div className="col-span-8 sm:col-span-8 lg:col-span-12">
                         <label htmlFor="state" className="block text-sm font-medium text-gray-700">
                           State
                         </label>
-                        <select
-                          type="text"
-                          name="state"
-                          id="state"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        >
-                          <option></option>
-                          <option> Andaman and Nicobar Islands</option>
-                          <option> Andhra Pradesh </option>
-                          <option> Arunachal Pradesh </option>
-                          <option> Assam </option>
-                          <option> Bihar </option>
-                          <option> Chandigarh </option>
-                          <option> Chhattisgarh </option>
-                          <option> Dadra and Nagar Haveli </option>
-                          <option> Daman and Diu </option>
-                          <option> Delhi </option>
-                          <option> Goa </option>
-                          <option> Gujarat </option>
-                          <option> Haryana </option>
-                          <option> Himachal Pradesh </option>
-                          <option> Jammu and Kashmir </option>
-                          <option> Jharkhand </option>
-                          <option> Karnataka </option>
-                          <option> Kerala </option>
-                          <option> Lakshadweep </option>
-                          <option> Madhya Pradesh </option>
-                          <option> Maharashtra </option>
-                          <option> Manipur </option>
-                          <option> Meghalaya </option>
-                          <option> Mizoram </option>
-                          <option> Nagaland </option>
-                          <option> Odisha </option>
-                          <option> Puducherry </option>
-                          <option> Punjab </option>
-                          <option> Rajasthan </option>
-                          <option> Sikkim </option>
-                          <option> Tamil Nadu </option>
-                          <option> Telangana </option>
-                          <option> Tripura </option>
-                          <option> Uttar Pradesh </option>
-                          <option> Uttarakhand </option>
-                          <option> West Bengal </option>
-                        </select>
-                      </div>
 
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-3">
-                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">
-                          City
-                        </label>
-                        <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        <Select
+                          placeholder="Select State"
+                          value={state}
+                          options={data}
+                          onChange={handleStateChange}
+                          getOptionLabel={(x) => x.stateName}
+                          getOptionValue={(x) => x.stateName}
+                          name="state"
+                          inputId="nen100101"
+                          instanceId="nen100202"
                         />
                       </div>
 
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-3">
+                      <div className="col-span-8 sm:col-span-8 lg:col-span-12">
+                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                          City
+                        </label>
+                        <Select
+                          placeholder="Select City"
+                          value={city}
+                          options={cityList}
+                          onChange={handleCityChange}
+                          getOptionLabel={(x) => x.cityName}
+                          getOptionValue={(x) => x.cityName}
+                          name="city"
+                          inputId="nen100101"
+                          instanceId="nen100202"
+                        />
+                      </div>
+                      <div className="col-span-8 sm:col-span-8 lg:col-span-12">
                         <label
                           htmlFor="blood_group"
                           className="block text-sm font-medium text-gray-700"
@@ -89,6 +78,7 @@ export default function BloodSeekerIndex() {
                           name="blood_group"
                           id="blood_group"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          
                         >
                           <option></option>
                           <option> O+ </option>
@@ -101,23 +91,28 @@ export default function BloodSeekerIndex() {
                           <option> AB- </option>
                         </select>
                       </div>
+
                     </div>
                   </div>
                   <div className="px-4 py-3 bg-white text-center sm:px-6">
                     <button
-                      type="submit"
+                      type="search"
                       className="inline-flex justify-center py-2 px-12 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     >
-                      Save
+                      Search
                     </button>
                   </div>
                 </div>
               </form>
             </div>
           </div>
+          
         </div>
+       
       </div>
-      
+     
+      <div className="py-4"></div>
+      <BloodSeeker />
     </>
   )
 }
